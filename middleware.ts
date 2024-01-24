8bed0f24ca1a71e7aeb0d91b2c2a1e0693ccf05a
bspraveen <billasujith77@gmail.com>
2024-01-24 23:24:10 +0530

    Add error page and middleware for non-admin users trying to access add-product page

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 import {
+  DEFAULT_ADMIN_ERROR_REDIRECT,
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
   authRoutes,
@@ -12,12 +13,21 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-
+  const user = req.auth?.user;
+  const isAdmin = user?.role === "ADMIN";
+  const isAdminRoute = nextUrl.pathname === "/add-product";
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAdminRoute) {
+    if (!isLoggedIn || !isAdmin) {
+      return Response.redirect(new URL(DEFAULT_ADMIN_ERROR_REDIRECT, nextUrl));
+    }
     return null;
   }
 
--- routes.ts
+++ routes.ts
@@ -4,4 +4,8 @@ export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
 
 export const apiAuthPrefix = "/api/auth";
 
+export const adminRoute = "/add-product";
+
+export const DEFAULT_ADMIN_ERROR_REDIRECT = "/add-product/error";
+
 export const DEFAULT_LOGIN_REDIRECT = "/";
